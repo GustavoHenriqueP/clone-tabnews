b97b986f64084bc7fd10be885568bec99f90829d
= <=>
2024-12-09 20:16:55 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Esse pedacinho de internet, que vai estar sempre disponível pelo link que você clicou, eu dedico a você meu amor, você mesmo, Isabelle s2.</h1>
+}
+
+export default Home;
