ca55749f9a875da9f0173f5aaddb2cc850219129
= <=>
2024-12-16 22:45:18 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprender e implementar, do zero, o https://www.tabnews.com.br. Feito no https://curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Esse pedacinho de internet, que vai estar sempre disponível pelo link que você clicou, eu dedico a você meu amor, você mesmo, Isabelle s2 s2.</h1>
+  return (
+    <h1>
+      Esse pedacinho de internet, que vai estar sempre disponível pelo link que
+      você clicou, eu dedico a você meu amor, você mesmo, Isabelle s2 s2.
+    </h1>
+  );
 }
 
 export default Home;
